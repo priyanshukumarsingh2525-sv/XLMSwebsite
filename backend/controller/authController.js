8ed65d7c0bc05df1c15ca8c1fbbfbee62d5875ea
priyanshukumarsingh2525-sv <priyanshukumarsingh2525@gmail.com>
2026-02-17 18:50:24 +0530

    Update authController.js

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -30,8 +30,8 @@ export const signUp = async (req,res) =>{
         let token = await genToken(user._id);
         res.cookie("token", token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"Strict",
+            secure:true,
+            sameSite:"none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
@@ -59,8 +59,8 @@ export const login = async (req,res) => {
         
         res.cookie("token", token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"Strict",
+            secure:true,
+            sameSite:"none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
@@ -166,8 +166,8 @@ export const googleAuth = async (req,res) =>{
         
         res.cookie("token", token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"Strict",
+            secure:true,
+            sameSite:"none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
